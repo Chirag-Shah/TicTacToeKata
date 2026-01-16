89c066d71b79adcb2c8726eaec34da24bf097773
chirag2.shah <chirag2.shah@ril.com>
2026-01-16 20:40:22 +0530

    Added test cases for winning rule Row

--- app/src/test/java/com/tdd/tictactoetddlearning/GameViewModelTest.kt
+++ app/src/test/java/com/tdd/tictactoetddlearning/GameViewModelTest.kt
@@ -34,4 +34,38 @@ class GameViewModelTest {
         viewModel.onEvent(UIEvent.Restart)
         assertEquals(null, viewModel.state.value.board[0])
     }
+
+    @Test
+    fun `test winner X First Row`() {
+        viewModel.onEvent(UIEvent.OnCellClicked(0)) // X
+        viewModel.onEvent(UIEvent.OnCellClicked(3)) // O
+        viewModel.onEvent(UIEvent.OnCellClicked(1)) // X
+        viewModel.onEvent(UIEvent.OnCellClicked(4)) // O
+        viewModel.onEvent(UIEvent.OnCellClicked(2)) // X
+
+        assertEquals('X', viewModel.state.value.winner)
+    }
+
+    @Test
+    fun `test winner X Second Row`() {
+        viewModel.onEvent(UIEvent.OnCellClicked(3)) // X
+        viewModel.onEvent(UIEvent.OnCellClicked(1)) // O
+        viewModel.onEvent(UIEvent.OnCellClicked(4)) // X
+        viewModel.onEvent(UIEvent.OnCellClicked(8)) // O
+        viewModel.onEvent(UIEvent.OnCellClicked(5)) // X
+
+        assertEquals('X', viewModel.state.value.winner)
+    }
+
+    @Test
+    fun `test winner O Third Row`() {
+        viewModel.onEvent(UIEvent.OnCellClicked(1)) // X
+        viewModel.onEvent(UIEvent.OnCellClicked(6)) // O
+        viewModel.onEvent(UIEvent.OnCellClicked(0)) // X
+        viewModel.onEvent(UIEvent.OnCellClicked(7)) // O
+        viewModel.onEvent(UIEvent.OnCellClicked(3)) // X
+        viewModel.onEvent(UIEvent.OnCellClicked(8)) // O
+
+        assertEquals('O', viewModel.state.value.winner)
+    }
 }
