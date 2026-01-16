0c66255928a9cb0f7cfe7b68d2bceaeee51318dc
chirag2.shah <chirag2.shah@ril.com>
2026-01-16 21:12:27 +0530

    Added gradle for viewmodel compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // ViewModel + Compose integration
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 }
