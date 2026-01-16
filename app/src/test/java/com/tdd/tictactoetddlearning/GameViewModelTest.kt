91ebaa4b7cc8319d1ba91419ca2b9d63fc811886
chirag2.shah <chirag2.shah@ril.com>
2026-01-16 09:39:54 +0530

    Added test case to test that User 'X' moves first.

--- app/src/test/java/com/tdd/tictactoetddlearning/GameViewModelTest.kt
+++ app/src/test/java/com/tdd/tictactoetddlearning/GameViewModelTest.kt
@@ -1,7 +1,11 @@
 package com.tdd.tictactoetddlearning
 
+import com.tdd.tictactoetddlearning.presentation.state.UIEvent
 import com.tdd.tictactoetddlearning.presentation.viewmodel.GameViewModel
+import junit.framework.TestCase.assertEquals
 import org.junit.Before
+import org.junit.Test
+import kotlin.collections.get
 
 class GameViewModelTest {
 
@@ -10,5 +14,11 @@ class GameViewModelTest {
     @Before
     fun setup() {
         viewModel = GameViewModel()
+    }
+
+    @Test
+    fun `test X moves first`() {
+        viewModel.onEvent(UIEvent.OnCellClicked(0))
+        assertEquals('X', viewModel.state.value.board[0])
     }
 }
