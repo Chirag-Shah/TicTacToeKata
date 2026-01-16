7456dffb68e6bb084622188debd21b5448569509
chirag2.shah <chirag2.shah@ril.com>
2026-01-16 09:51:14 +0530

    edit logic to pass the test cases for user move and toggles player

--- app/src/main/java/com/tdd/tictactoetddlearning/presentation/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/tdd/tictactoetddlearning/presentation/viewmodel/GameViewModel.kt
@@ -5,6 +5,8 @@ import com.tdd.tictactoetddlearning.presentation.state.GameState
 import com.tdd.tictactoetddlearning.presentation.state.UIEvent
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlin.collections.get
+import kotlin.text.set
 
 class GameViewModel : ViewModel() {
 
@@ -15,7 +17,32 @@ class GameViewModel : ViewModel() {
      * On Event which is called bye the view when user tap on board or restart game
      */
     fun onEvent(event: UIEvent) {
-       //To-Do
+        when (event) {
+            is UIEvent.OnCellClicked -> handleTap(event.index)
+            UIEvent.Restart -> { //To-Do
+            }
+        }
     }
 
+    /**
+     * Handle tap which checks for winning/dra conditions
+     */
+    private fun handleTap(index: Int) {
+        val s = _state.value
+
+        val newBoard = s.board.toMutableList()
+        newBoard[index] = s.currentPlayer
+
+        _state.value = s.copy(
+            board = newBoard,
+            currentPlayer = togglePlayer(s.currentPlayer),
+            winner = null,
+            isDraw = false
+        )
+    }
+
+    /**
+     * Toggle player X and O
+     */
+    private fun togglePlayer(c: Char) = if (c == 'X') 'O' else 'X'
 }
